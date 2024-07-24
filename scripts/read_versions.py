d4b37d7e8f79c5d581d4a6e71ef0889afa0da18f
Hsu, Shou-Li <shou-li.hsu@intel.com>
2024-07-24 06:52:57 -0700

    Add the jobs summary

--- scripts/read_versions.py
+++ scripts/read_versions.py
@@ -7,3 +7,9 @@ with open('versions.json', 'r') as file:
 # Write the versions to the GitHub output environment file
 with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
     output_file.write(f"matrix={json.dumps(versions['versions'])}\n")
+
+# Write the summary to the GITHUB_STEP_SUMMARY file
+with open(os.environ['GITHUB_STEP_SUMMARY'], 'a') as summary_file:
+    summary_file.write("## Versions to be built 🚀\n")
+    for version in versions['versions']:
+        summary_file.write(f"- {version}\n")
